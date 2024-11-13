3780a39ef017b4ec7275ebb1c2451a63902f1f1b
Prateek <92998340+kprateek87@users.noreply.github.com>
2024-11-13 10:18:01 +0530

    Search Functionality

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -33,13 +33,14 @@ function Inputs({ setQuery, units, setUnits }) {
   return (
     <div className="flex flex-row justify-center my-6">
       <div className="flex flex-row w-3/4 items-center justify-center md:space-x-4">
+        <form onSubmit={handleSearchClick}>
         <input
           value={city}
           onChange={(e) => setCity(e.currentTarget.value)}
           type="text"
           placeholder="Search for city...."
           className="bg-transparent/60 text-white text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase placeholder:text-white/40"
-        />
+        /></form >
         <CiSearch
           size={25}
           className="text-white cursor-pointer transition ease-out hover:scale-125"
